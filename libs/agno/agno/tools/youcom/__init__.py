2e36fdb1835ff6f12ded9e2800a1886e083b8f7e
anshul-jain-devx108 <anshul.jain.devx@gmail.com>
2026-07-17 21:44:26 +0530

    feat(tools): export YouFinanceResearchTools in youcom package

--- libs/agno/agno/tools/youcom/__init__.py
+++ libs/agno/agno/tools/youcom/__init__.py
@@ -1,5 +1,6 @@
 from agno.tools.youcom.contents import YouContentsTools
+from agno.tools.youcom.finance import YouFinanceResearchTools
 from agno.tools.youcom.research import YouResearchTools
 from agno.tools.youcom.search import YouTools
 
-__all__ = ["YouContentsTools", "YouResearchTools", "YouTools"]
+__all__ = ["YouContentsTools", "YouFinanceResearchTools", "YouResearchTools", "YouTools"]
